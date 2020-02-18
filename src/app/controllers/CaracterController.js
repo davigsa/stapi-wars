3b4bc21819ce520b545d52515e43c047ca61adb2
Davi <davi.goulartsa@gmail.com>
2020-02-18 10:12:31 -0300

    implementing eslint and prettier

--- src/app/controllers/CaracterController.js
+++ src/app/controllers/CaracterController.js
@@ -1,57 +1,72 @@
-import Caracter from '../models/Caracter';
+/* eslint-disable consistent-return */
 import axios from 'axios';
 import { next } from 'sucrase/dist/parser/tokenizer';
+import Caracter from '../models/Caracter';
 
 class CaracterController {
-    async index(req, res) {
-        const caracters = await Caracter.findAll();
+  async index(req, res) {
+    const caracters = await Caracter.findAll();
 
-        return caracters ? res.json(caracters) : res.status(404).json({ error: 'This database is empty, please run the store method to fill it.' });
-    }
-
-    async store(req, res) {
-            const swapiResponse = await axios.get('https://swapi.co/api/people/');
-
-            await swapiResponse.data.results.map(async (result) => {
-                const { name, height, mass, hair_color, skin_color, eye_color, birth_year, gender, homeworld } = result;
-                const _caracter = {
-                    name,
-                    height, 
-                    mass, 
-                    hair_color, 
-                    skin_color, 
-                    eye_color, 
-                    birth_year, 
-                    gender, 
-                    homeworld
-                };
-
-                try {
-                    const caracter = await Caracter.create(_caracter);
-                    return res.json(caracter);
-
-                } catch (e) {
-                    console.log(_caracter);
-                    console.log(e);
-                    next(e);
-
-                }
-            });
-    }
-
-    async show(req, res) {
-        const caracter = await Caracter.findOne({
-            where: {
-                id: req.params.id
-            }
+    return caracters
+      ? res.json(caracters)
+      : res.status(404).json({
+          error:
+            'This database is empty, please run the store method to fill it.',
         });
+  }
 
-        if(caracter) {
-            return res.json(caracter);
-        } else {
-            return res.status(404).json({ error: 'This caracter could not be found, please be free to create this on the database.' })
-        }
+  async store(req, res) {
+    const swapiResponse = await axios.get('https://swapi.co/api/people/');
+
+    await swapiResponse.data.results.map(async result => {
+      const {
+        name,
+        height,
+        mass,
+        hair_color,
+        skin_color,
+        eye_color,
+        birth_year,
+        gender,
+        homeworld,
+      } = result;
+
+      const caracter = {
+        name,
+        height,
+        mass,
+        hair_color,
+        skin_color,
+        eye_color,
+        birth_year,
+        gender,
+        homeworld,
+      };
+
+      try {
+        const caracters = await Caracter.create(caracter);
+        return res.json(caracters);
+      } catch (e) {
+        next(e);
+      }
+    });
+  }
+
+  async show(req, res) {
+    const caracter = await Caracter.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (caracter) {
+      return res.json(caracter);
     }
+    return res.status(404).json({
+      error:
+        'This caracter could not be found, please be free to create this on the database.',
+    });
+  }
 }
 
 export default new CaracterController();
